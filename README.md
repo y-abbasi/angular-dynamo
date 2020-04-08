de0ef1e73a36dba90ac7e517009a8ee7a0a95a2f
y-abbasi <y.abbasi.62@gmail.com>
2020-04-08 19:12:51 +0430

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-dynamo
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-dynamo)
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule } from "@angular/router";
+import { ReactiveFormsModule } from "@angular/forms";
+
+import { AppComponent } from "./app.component";
+import { TopBarComponent } from "./top-bar/top-bar.component";
+import { ProductTypeComponent } from "./product-type/product-type.component";
+var routes = [
+  {
+    path: "",
+    component: ProductTypeComponent
+  }
+];
+@NgModule({
+  imports: [BrowserModule, RouterModule.forRoot(routes), ReactiveFormsModule],
+  declarations: [AppComponent, TopBarComponent, ProductTypeComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
