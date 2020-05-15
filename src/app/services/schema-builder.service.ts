d2f63a2242b2460f55be43ddf74570de2f3b2889
y.abbasi <y.abbasi.62@gmail.com>
2020-05-15 12:38:04 +0430

    get properties of object properly in schemabuilder

--- src/app/services/schema-builder.service.ts
+++ src/app/services/schema-builder.service.ts
@@ -4,12 +4,15 @@ import { DesignerBase } from "../model/designers/designer-base";
 
 @Injectable()
 export class SchemaBuilderService {
-  constructor() {}
+  constructor() { }
   create(obj: any): Array<FieldBase> {
+    var props = Object.getOwnPropertyNames(obj)
+      .concat(Object.getOwnPropertyNames(obj.constructor.prototype))
+      .reduce((a, b) => { a[b] = b; return a; }, {})
     let res = new Array<FieldBase>();
-    for(var p in obj){
-      let meta = <DesignerBase> Reflect.getMetadata("design:type", obj, p);
-      if(meta){
+    for (var p in props) {
+      let meta = <DesignerBase>Reflect.getMetadata("design:type", obj, p);
+      if (meta) {
         res.push(meta.getField(p));
       }
     }
